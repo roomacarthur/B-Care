c72e82b5d1ab05a27d76ae5ab935280f68ceb093
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-11-12 22:36:40 +0000

    Add 500.html page and respective app.py code

--- app.py
+++ app.py
@@ -130,3 +130,8 @@ if __name__ == '__main__':
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
+#Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
